498902a8bed5436c4cc82b1d7d9d49b96bef021f
shanmathi gunasekaran <shanmathi7215@gmail.com>
2024-04-02 00:28:17 -0400

    add light and dark themes, create values-dark

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -1,21 +1,53 @@
 package com.example.spotifywrapped;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.example.spotifywrapped.XMPPTask;
+import androidx.appcompat.app.AppCompatDelegate;
+import android.widget.CompoundButton;
+import androidx.appcompat.widget.SwitchCompat;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
     private EditText messageEditText;
     private XMPPTask xmppTask;
     private TextView messageTextView;
+    SwitchCompat switchCompat;
+    SharedPreferences sharedPreferences = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchCompat = findViewById(R.id.switchCompat);
+        sharedPreferences = getSharedPreferences("night",0);
+        Boolean booleanValue = sharedPreferences.getBoolean("night_mode",true);
+        if (booleanValue){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchCompat.setChecked(true);
+        }
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchCompat.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",true);
+                    editor.commit();
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchCompat.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",false);
+                    editor.commit();
+
+                }
+            }
+        });
+
         messageEditText = findViewById(R.id.messageEditText);
         Button sendButton = findViewById(R.id.sendButton);
         messageTextView = findViewById(R.id.messageTextView);
