df87727b4d9c9a1f42e48e4e1df52e8eec72d023
tsar-boomba <itg.2048@gmail.com>
2024-04-11 16:56:42 -0400

    merge main

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.spotifywrapped;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.room.Room;
+
+import com.example.spotifywrapped.spotify.Spotify;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AppDatabase db;
+    private final Spotify spotify = new Spotify();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(this, AppDatabase.class, "db").build();
+
+        SwitchCompat switchCompat = findViewById(R.id.switchCompat);
+        SharedPreferences sharedPreferences = getSharedPreferences("night",0);
+        boolean booleanValue = sharedPreferences.getBoolean("night_mode",true);
+        if (booleanValue){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchCompat.setChecked(true);
+        }
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchCompat.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",true);
+                    editor.apply();
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchCompat.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",false);
+                    editor.apply();
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        spotify.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public Spotify getSpotify() {
+        return spotify;
+    }
+
+    public AppDatabase getDb() {
+        return db;
+    }
+
+    public void getCode() {
+        spotify.getCode(this);
+    }
+
+    public void getToken() {
+        spotify.getToken(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+        db = null;
     }
 }
